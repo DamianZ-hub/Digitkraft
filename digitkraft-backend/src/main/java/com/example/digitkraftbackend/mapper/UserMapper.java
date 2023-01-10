14d500a2c2ef5a42fed708e258384b49c78456eb
damia <damian.zam1f20@gmail.com>
2023-01-10 22:21:36 +0100

    Revert "Added:"
    This reverts commit e8f60a0817480a673c857f1595e8c5d6d5daa62b.

--- digitkraft-backend/src/main/java/com/example/digitkraftbackend/mapper/UserMapper.java
+++ digitkraft-backend/src/main/java/com/example/digitkraftbackend/mapper/UserMapper.java
@@ -1,10 +1,7 @@
 package com.example.digitkraftbackend.mapper;
 
 import com.example.digitkraftbackend.dto.ContactInfoDTO;
-import com.example.digitkraftbackend.dto.OrderDTO;
 import com.example.digitkraftbackend.dto.RegisterUserDTO;
-import com.example.digitkraftbackend.dto.UserDTO;
-import com.example.digitkraftbackend.model.Order;
 import com.example.digitkraftbackend.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -15,6 +12,4 @@ public interface UserMapper {
     @Mapping(source = "email", target = "contactInfo.email")
     @Mapping(constant = "true", target = "enabled")
     User registerUserDTOToUser(RegisterUserDTO registerUserDTO);
-
-    UserDTO UserToUserDTO(User user);
 }
